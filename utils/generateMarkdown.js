ee0fbea9a059c3df116d3302d46876df4523751e
muulinha <lucasfl.araujo@hotmail.com>
2023-04-19 13:48:21 +0800

    created function for the markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,7 +24,41 @@ function renderLicenseSection(license) {
 }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` # ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+## DESCRIPTION
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contribution](#contribution)
+* [Test](#test)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contribution
+${data.contribution}
+
+## Test
+${data.test}
+
+
+## Questions
+
+To reach me for any questions contact me at my:
+
+Github: [${data.github}](https://github.com/${data.github})
+E-mail:${data.email}
 
 `;
 }
