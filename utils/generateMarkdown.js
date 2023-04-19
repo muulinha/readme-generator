dde36e0b9f646e6ed08edcb6211bbfdd9230b3ed
muulinha <lucasfl.araujo@hotmail.com>
2023-04-19 13:47:19 +0800

    Created render for the license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,12 +6,13 @@ function renderLicenseBadge(license) {
   return `![Github badge](https://img.shields.io/badge/license-${license}-blue)`
 }
 
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return ''
+  }
+  return `* [License](#license)`
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
