55f9bc1a9edc006fad78ea149a60e55cc737c267
muulinha <lucasfl.araujo@hotmail.com>
2023-04-19 13:37:38 +0800

    Questions array created.

--- index.js
+++ index.js
@@ -5,7 +5,56 @@ const generateMarkdown = require('./assets/utills/generateMarkdown');
 const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        name:'title',
+        message:"what is the project title?"
+    },
+    {
+        type:'input',
+        name:'description',
+        message:"what is the description of your project?"
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:"what is the installation for your project?"
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:"what is the usage information of your project?"
+    },
+    {
+        type:'list',
+        name:'license',
+        message:"what is the license of your project?",
+        choices:['ISC', 'Apache license', 'MIT license', 'None']
+    },
+    {
+        type:'input',
+        name:'contribution',
+        message:"what is the contributing guidelines of your project?"
+    },
+    {
+        type:'input',
+        name:'test',
+        message:"what is the test instruction of your project?"
+    },
+    {
+        type:'input',
+        name:'github',
+        message:"what is your github username?"
+    },
+    {
+        type:'input',
+        name:'email',
+        message:"what is your email?"
+    },
+
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
